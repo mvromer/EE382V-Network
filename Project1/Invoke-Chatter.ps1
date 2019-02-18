1b2fc7ef94b4a54992c1421d095cb033aaa6b178
Michael Romer <mvromer@gmail.com>
2019-02-17 20:26:45 -0600

    Added check for correct version of Python

--- Project1/Invoke-Chatter.ps1
+++ Project1/Invoke-Chatter.ps1
@@ -26,16 +26,26 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+# Make sure we have Python.
 $python = Get-Command python -ErrorAction Ignore
 if( -not $python ) {
     throw "Missing Python. Must install Python 3.7 to run this client."
 }
 
+# Make sure we have the required version of Python.
+$installedVersion = & $python -c "from sys import version_info as v; print( '%s.%s' % (v.major, v.minor) )"
+if( $installedVersion -ne "3.7" ) {
+    throw ("Wrong version of Python found (version found: $installedVersion). " +
+        "Ensure Python 3.7 is the first available version on the path.")
+}
+
+# Make sure we have pip.
 $pip = Get-Command pip -ErrorAction Ignore
 if( -not $pip ) {
     & $python -m ensurepip
 }
 
+# Make sure we have pipenv.
 $pipenv = Get-Command pipenv -ErrorAction Ignore
 if( -not $pipenv ) {
     pip install pipenv
@@ -43,6 +53,7 @@ if( -not $pipenv ) {
 
 Push-Location $PSScriptRoot
 try {
+    pipenv install
     pipenv run python client.py $ScreenName $ServerAddress $ServerPort
 }
 finally {

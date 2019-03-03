6a5f9714d71a4c9eb44b77c695f8a272b469b7c4
Michael Romer <mvromer@gmail.com>
2019-03-03 13:57:16 -0600

    Added connection management

--- Project2/server.py
+++ Project2/server.py
@@ -4,6 +4,8 @@ import platform
 import selectors
 import signal
 import sys
+import warnings
+import weakref
 
 class HELO:
     def __init__( self, screen_name, client_ip, client_port ):
@@ -15,23 +17,100 @@ class HELO:
     def new( cls, data ):
         return cls( *data.split( " " ) )
 
-def parse_message( message ):
-    message_type, _, message_data = message.partition( " " )
+class EXIT:
+    @classmethod
+    def new( cls, data ):
+        return cls()
 
-    if message_type == "HELO":
-        return HELO.new( message_data )
+def parse_message( message ):
+    try:
+        message_type, _, message_data = message.partition( " " )
+
+        if message_type == "HELO":
+            return HELO.new( message_data )
+        elif message_type == "EXIT":
+            return EXIT.new( message_data )
+    except:
+        # If the message wasn't anything we recognized or was badly formatted, then we just drop it.
+        pass
 
 class Member:
     pass
+
+class MemberConnection( asyncio.Protocol ):
+    def __init__( self, server ):
+        self._server = weakref.proxy( server )
+        self._transport = None
+        self._message_chunks = []
+        self._conn_id = None
+
+    def connection_made( self, transport ):
+        self._conn_id = transport.get_extra_info( "sockname" )
+        print( f"Connection made: {self._conn_id}" )
+        self._server.register_connection( self._conn_id )
+
+    def connection_lost( self, ex ):
+        print( "Connection lost" )
+        self._server.unregister_connection( self._conn_id )
+
+    def data_received( self, data ):
+        print( "Data received" )
+        for message in self._feed_data( data ):
+            print( f"New message: {message}")
+            message = parse_message( message )
+            if message:
+                self._server.create_task( self._server.handle_message( message, self._conn_id ) )
+
+    def _feed_data( self, data ):
+        # We keep feeding chunks until one contains at least one newline character. The presence of
+        # a newline indicates that at least one complete message has arrived. In this case, we want
+        # to find the last newline in the current chunk and preserve everything after it as the
+        # start of the next message that has yet to be completed.
+        #
+        # For everything before the last newline, this gets concatenated with the other message
+        # chunks previously stored. The concatenated string is then split across newline boundaries
+        # (since it may be the rare case there are mutliple messages buffered up), and each portion
+        # corresponds to a protocol message that needs parsing.
+        #
+        chunk = data.decode()
+        left_chunk, newline, right_chunk = chunk.rpartition( "\n" )
+
+        if newline:
+            # There was a newline in the latest chunk. The one we partitioned on is either at the
+            # beginning of our chunk (in which case left_chunk is empty), it's at the end of our
+            # chunk (in which case right_chunk is empty), or it's somewhere in the middle.
+
+            if left_chunk:
+                # There is content prior to the newline we partitioned on. The preceding content
+                # needs to be appened to the list of chunks that will be concatenated and parsed.
+                self._message_chunks.append( left_chunk )
+
+            # Concatenate all of our message chunks into one large string. Split the string on
+            # message boundaries (newlines) and yield each message found.
+            for message in "".join( self._message_chunks ).split( "\n" ):
+                yield message
+
+            # Discard the chunks we've exhausted in terms of messages that can be parsed.
+            self._message_chunks = []
+
+            if right_chunk:
+                # There is content following the newline we partitioned on. This content forms the
+                # first chunk save in the new set of message chunks.
+                self._message_chunks.append( right_chunk )
+        else:
+            # There was no newline in the current chunk. Add it to our list of message chunks.
+            self._message_chunks.append( chunk )
 
 class Server:
     def __init__( self, port ):
         self._port = port
         self._server = None
+        self._connections = {}
         self._members = []
 
     async def run( self ):
-        self._server = await asyncio.start_server( self._on_client_connected, port=self._port )
+        loop = asyncio.get_running_loop()
+        self._server = await loop.create_server( lambda: MemberConnection( self ), port=self._port )
 
         # Windows requires a dirty hack to ensure the keyboard interrupt (Ctrl+C) results in the
         # event loop shutting down, even when it would normally be in an idle state (for instance,
@@ -42,7 +121,6 @@ class Server:
         # Learned about this workaround from here: https://stackoverflow.com/a/36925722/562685
         #
         if platform.system() == "Windows":
-            loop = self._server.get_loop()
             loop.create_task( self._wakeup() )
 
         async with self._server:
@@ -55,15 +133,24 @@ class Server:
         await self._server.wait_closed()
         print( "Server closed" )
 
-    async def _on_client_connected( self, reader, writer ):
-        print( "Client connected" )
-        message = await reader.readline()
-        print( f"Read: {message}" )
-        writer.close()
-        await writer.wait_closed()
+    def create_task( self, coro ):
+        self._server.get_event_loop().create_task( coro )
 
-    def _handle_message( self, message ):
+    def register_connection( self, conn_id ):
+        if conn_id in self._connections:
+            warnings.warn( f"Connection already registered: {conn_id}" )
+            return
+
+        self._connections[conn_id] = Member()
+
+    def unregister_connection( self, conn_id ):
+        if conn_id in self._connections:
+            del self._connections[conn_id]
+
+    async def handle_message( self, message, conn_id ):
         if isinstance( message, HELO ):
+            pass
+        elif isinstance( message, EXIT ):
             pass
 
     async def _wakeup( self ):
